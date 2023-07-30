10d2a904400f8fc9e5ae40af2dcaceb6cb8eef41
tresna <tresna312@gmail.com>
2023-07-30 18:08:31 +0600

    dashboard

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SideBar</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Styles -->
+      
+           <link href="{{asset('css/style.css')}}" rel="stylesheet" />
+           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+    </head>
+    <body >
+    <div class="sidebar">
+    <h1 class="head" style="margin-top:-15px">Admin</h1> 
+  <a href="#home"><i class="fa fa-tachometer"></i>Dashboard</a>
+  <a href="#users"><i class="fa-solid fa-user"></i>Users</a>
+  <a href="#users"><i class="fa-solid fa-people-group"></i>Group</a>
+  <a href="#users"><i class="fa-sharp fa-solid fa-tags"></i></i>Brands</a>
+  <a href="#users"><i class="fa-sharp fa-solid fa-arrows-to-eye"></i>Category</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Stores</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Attributes</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Products</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Order</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Reports</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Company</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>profile</a>
+  <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
+  <a href="#users"><i class="bi bi-gear"></i>Setting</a>
+  <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+  <a href="#users"><i class="fa fa-fw fa-home"></i>Logout</a>
+</div>
+
+
+<!-- As a heading -->
+<nav class="navbar">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0">Navbar</span>
+  </div>
+</nav>
+</nav>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    </body>
+   
+</html>
